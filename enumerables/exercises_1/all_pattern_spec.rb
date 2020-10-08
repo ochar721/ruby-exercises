83b590e256a2f3d5a9feeb8c6f430cc194322655
memcmahon <mcmahon.meganelizabeth@gmail.com>
2020-10-07 21:27:49 -0600

    enums wip

--- enumerables/exercises_1/all_pattern_spec.rb
+++ enumerables/exercises_1/all_pattern_spec.rb
@@ -1,89 +1,74 @@
-gem 'minitest'
-require 'minitest/autorun'
-require 'minitest/pride'
-
-class AllPatternTest < Minitest::Test
-
-  def test_all_zeros
+RSpec.describe "#all pattern" do
+  it "all zeros" do
     numbers = [0, 0, 0, 0, 0, 0, 0]
     all_zeros = true
     numbers.each do |number|
       all_zeros = false unless number.zero?
     end
-    assert all_zeros
+    expect(all_zeros).to eq(true)
   end
 
-  def test_not_all_zeros
+  it "not all zeros" do
     numbers = [0, 0, 0, 0, 1, 0, 0, 0]
-    all_zeros = true
+    all_zeros = true # Do not change this line!
     numbers.each do |number|
       # Your code goes here
     end
-    refute all_zeros
+    expect(all_zeros).to_not eq(true)
   end
 
-  def test_all_gone
-    skip
+  xit "all gone" do
     words = ["gone", "gone", "gone", "gone", "gone", "gone", "gone"]
     all_gone = true
-    # Your code goes here
-    assert all_gone
+    # Your code goes here (you must do some code here 😉)
+    expect(all_gone).to eq(true)
   end
 
-  def test_not_all_gone
-    skip
+  xit "not all gone" do
     words = ["gone", "gone", "gone", "gone", "gone", "there", "gone", "gone"]
     # Your code goes here
-    refute all_gone
+    expect(all_gone).to_not eq(true)
   end
 
-  def test_all_empty
-    skip
+  xit "all empty" do
     strings = ["", "", "", "", "", "", ""]
     # Your code goes here
-    assert all_empty
+    expect(all_empty).to eq(true)
   end
 
-  def test_not_all_empty
-    skip
+  xit "not all empty" do
     strings = ["", "", "", "full", "", "", ""]
     # Your code goes here
-    refute all_empty
+    expect(all_empty).to_not eq(true)
   end
 
-  def test_not_all_uppercase
-    skip
+  xit "not all uppercase" do
     words = ["DOUGHNUT", "CASH", "MAIN", "bOWl", "SMACK", "SAND"]
     # Your code goes here
-    refute all_caps
+    expect(all_caps).to_not eq(true)
   end
 
-  def test_all_lies
-    skip
+  xit "all_lies" do
     lies = [false, false, false, false]
     # Your code goes here
-    assert all_lies
+    expect(all_lies).to eq(true)
   end
 
-  def test_all_multiples_of_7
-    skip
+  xit "all multiples of 7" do
     numbers = [42, 14, 35, 49, 28, 56, 21, 7]
     # Your code goes here
-    assert all_multiples_of_7
+    expect(all_multiples_of_7).to eq(true)
   end
 
-  def test_not_all_3_digits_long
-    skip
+  xit "not all 3 digits long" do
     numbers = [981, 831, 509, 332, 892, 8999, 110]
     # Your code goes here
-    refute all_3_digits
+    expect(all_3_digits).to_not eq(true)
   end
 
-  def test_all_4_letter_words
-    skip
+  xit "all 4 letter words" do
     words = ["love", "hate", "fire", "bird", "call"]
     # Your code goes here
-    assert all_4_letters
+    expect(all_4_letters).to eq(true)
   end
-
 end
